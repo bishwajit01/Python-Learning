409e2a0e0bbfbe562c24efd30b644039beabfa7f
bishwajit01 <bishwajit.vikram@gmail.com>
2020-01-26 19:19:14 +0530

    Day 2 python Data types and Identifiers.

---
+++ PythonIntroduction/day2/Part1.py
@@ -0,0 +1,26 @@
+'''
+Created on Jan 26, 2020
+
+@author: Bishwajit
+'''
+
+import keyword
+print(keyword.kwlist)
+
+a = 10  # a is identifier
+# class Employee # B is in identifier
+# def function_name: # function_name is an identifier
+
+'''
+Rules for defining an identifier
+1. Must begin with alphabets
+2. can have 0 to 9 numbers
+3. Should not match with Keyword
+4. Should not separate with space.
+5. Special symbols aren't allowed.
+'''
+
+# Single Line Comments in Python.s
+'''
+MultiLine Comment
+'''
---
+++ PythonIntroduction/day2/Part2.py
@@ -0,0 +1,42 @@
+'''
+Created on Jan 26, 2020
+
+@author: Bishwajit
+'''
+
+'''
+Data Types in Python :
+Fundamental Data Types
+    Int
+    Float
+    Complex
+    Bool
+    Str
+Secondary Data Types
+    List
+    Tuple
+    Set
+    dict
+'''
+
+# int Data Type
+a = 1
+print(type(a))
+
+# float Data Type
+b = 1.4
+print(type(b))
+
+# Complex Data Type
+# Represent in python a + bj
+c = 10 + 2j
+print(type(c))
+
+# Boolean (True/False)
+d = True
+print(type(d))
+
+print(True + True)
+print(True + False)
+print(False + True)
+print(False + False)
