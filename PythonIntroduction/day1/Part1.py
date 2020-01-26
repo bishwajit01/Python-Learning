9053bd0adfcba45ca378dcf84d03b3139b88edb3
bishwajit01 <bishwajit.vikram@gmail.com>
2020-01-26 17:36:28 +0530

    Different set of displaying numbers and string

--- PythonIntroduction/day1/Part1.py
+++ PythonIntroduction/day1/Part1.py
@@ -6,10 +6,17 @@ Created on Jan 26, 2020
 
 print("Hello World")
 
-
 a = 10
 print(a)
 
+print(3 + 4)
+print('3' + '4')
 
-print(3+4)
-print('3'+'4')
+print(a * a)  # Multiply
+print(a * 2)
+print(a ** a)  # Exponential Power
+
+a = "apple"
+print(a)
+print(a * 2)  # it displays number of times
+
