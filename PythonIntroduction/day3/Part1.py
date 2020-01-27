4772ecc45a03ffe06fe1399fc6d6c423096f402b
bishwajit01 <bishwajit.vikram@gmail.com>
2020-01-27 12:07:11 +0530

    String Introduction.

---
+++ PythonIntroduction/day3/Part1.py
@@ -0,0 +1,26 @@
+'''
+Created on Jan 26, 2020
+
+@author: Bishwajit
+'''
+
+# STRINGS
+# String Objects are Immutables.
+# Insertion order are preserved in Strings.
+# It allows multiple duplicate Values.
+
+a = 'apple'
+b = "apples"
+
+# Mutliline Strings
+c = '''Apples
+one more apple''' 
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
