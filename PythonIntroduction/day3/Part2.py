d1f6b27fad4dff82c107ae735e6c6711590b9e5f
bishwajit01 <bishwajit.vikram@gmail.com>
2020-01-27 12:07:44 +0530

    Python Indexing.

---
+++ PythonIntroduction/day3/Part2.py
@@ -0,0 +1,21 @@
+'''
+Created on Jan 27, 2020
+
+@author: Bishwajit
+'''
+'''
+String Operations :
+1. Indexing
+2. SLicing
+3. Concatenation
+4. Multiplication
+'''
+
+# Indexing
+a = "apples"
+print(a[0])
+print(a[3])
+print(a[-2])
+print(a[4])
+
+# Slicing
