b3a18c814b8d69446488ca001906fb7e26d65195
bishwajit01 <bishwajit.vikram@gmail.com>
2020-01-29 11:59:36 +0530

    Tuples properties and operations.

---
+++ PythonIntroduction/day6/Part1.py
@@ -0,0 +1,40 @@
+'''
+Created on Jan 28, 2020
+
+@author: bvikram2
+'''
+
+'''
+Tuples
+    Same as List
+    It is immutable.
+    It is enclosed by ()
+    It is separated by Comma(,)
+    It can store heterogeneous elements.
+'''
+
+a = (1, True, 4.8, "Hello")
+print(a)
+print(type(a))
+
+b = 21, True, 4.8, "Hello"  # By default it acts as Tuple
+print(type(b))
+
+# Indexing
+print(a[0])
+print(b[1])
+print(b[2])
+print(b[-1])
+
+# Slicing
+print(b[1:])
+print(b[::-1])
+print(b[1:3])
+
+# Concatenation
+c = a + b
+print(c)
+
+# Multiplication
+print(a * 2)
+print(a[3] * 2)
