b6df00ba63822cca80023e2b3dde49edfeba2960
bishwajit01 <bishwajit.vikram@gmail.com>
2020-01-31 00:08:54 +0530

    Conditional Statement

---
+++ PythonIntroduction/day8/Part1.py
@@ -0,0 +1,29 @@
+'''
+Created on Jan 30, 2020
+
+@author: bvikram2
+'''
+'''
+Control Flow Statement
+    Condition statements
+        If statement
+        If else statement
+        If elif else statement
+        Nested If statement
+    Looping Statements
+    Transfer Statement.
+'''
+
+# CONDITIONAL STATEMENT
+
+a = int(input("Enter a number"))
+
+if a > 0:  # if statement
+    if a >= 10:
+        print("Greater than 10")
+    else:
+        print("Positive Number")
+elif a == 0:  # elif statement
+    print("Equal to Zero")
+else:  # else statement
+    print("Negative Number")
