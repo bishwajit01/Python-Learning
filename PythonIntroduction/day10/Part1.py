3abc5f5088231b39444e979113805b1e37ebb126
bishwajit01 <bishwajit.vikram@gmail.com>
2020-02-03 21:26:55 +0530

    Exception Handling in Python.

---
+++ PythonIntroduction/day10/Part1.py
@@ -0,0 +1,19 @@
+'''
+Created on Feb 3, 2020
+
+@author: bvikram2
+'''
+'''
+Exception Handling
+    Errors
+        Syntax error
+        RunTime Error
+Exception Handling Using Try Except and Finally
+'''
+
+# if a > 2 # Syntax Error
+
+a, b = 20, 0
+c = a / b  # Runtime Exception
+print(c)
+
---
+++ PythonIntroduction/day10/Part2.py
@@ -0,0 +1,43 @@
+'''
+Created on Feb 3, 2020
+
+@author: bvikram2
+'''
+'''
+Exception Handling Using 
+    Try
+    Except
+    Finally
+'''
+
+# try-catch block
+a, b = 20, 0
+try :
+    c = a / b
+    print(c)
+except:
+    print("Error Occurred")
+
+# try-catch block
+try :
+    c = a / b
+    print(c)
+except ZeroDivisionError:
+    print("Denominator should not be zero")
+
+# try-catch block
+a, b = 20, "a"
+try :
+    c = a / b
+    print(c)
+except TypeError:
+    print("Only Numbers")
+ 
+# try-catch-finally block 
+a, b = 20, "a"
+try :
+    c = a / b
+    print(c)
+except TypeError:
+    print("Only Numbers")
+ 
