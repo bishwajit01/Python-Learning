0d1ada7418be5f37cef416e94d09b29a80a67323
bishwajit01 <bishwajit.vikram@gmail.com>
2020-02-11 22:15:11 +0530

    oops concept and class creation in python.

---
+++ PythonIntroduction/day13/Part1.py
@@ -0,0 +1,21 @@
+'''
+Created on Feb 10, 2020
+
+@author: bvikram2
+'''
+'''
+OOPS Concept
+'''
+
+
+class className:
+    varName = "python"
+
+    def methodName(self):
+        print("HI")
+        print("Python :: ", self.varName)
+
+
+a = className()
+print(a.varName)
+a.methodName()
