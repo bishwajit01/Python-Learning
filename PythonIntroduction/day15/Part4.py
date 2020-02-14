6ccd4256ff5e60ddfafd9420b821b9019eff5fca
bishwajit01 <bishwajit.vikram@gmail.com>
2020-02-14 14:50:43 +0530

    Multiple Inheritance in python

---
+++ PythonIntroduction/day15/Part4.py
@@ -0,0 +1,32 @@
+'''
+Created on Feb 14, 2020
+
+@author: bvikram2
+'''
+'''
+Multiple Inheritance
+'''
+
+
+class A:
+
+    def m1(self):
+        print("A")
+
+        
+class B:
+
+    def m2(self):
+        print("B")
+
+
+class C(A, B):
+
+    def m3(self):
+        print("C")
+
+        
+ob = C()
+ob.m1()
+ob.m2()
+ob.m3()
