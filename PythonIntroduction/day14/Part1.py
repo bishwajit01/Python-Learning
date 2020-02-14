78071079add809b8247210cb272d094dec813601
bishwajit01 <bishwajit.vikram@gmail.com>
2020-02-14 14:28:37 +0530

    class variables, Instance variables and Constructor in python.

---
+++ PythonIntroduction/day14/Part1.py
@@ -0,0 +1,26 @@
+'''
+Created on Feb 14, 2020
+
+@author: bvikram2
+'''
+'''
+static variables/class variables
+Non-static variables/instance variables
+'''
+
+
+class PythonClass:
+    a = 1000
+
+    def methodName(self):
+        self.b = 100
+        print(self.a)
+        print(self.b)
+        print(PythonClass.a)
+        a = 10
+        print(a)
+
+        
+ob = PythonClass()
+ob.methodName()
+# print(ob.a)
---
+++ PythonIntroduction/day14/Part2.py
@@ -0,0 +1,23 @@
+'''
+Created on Feb 14, 2020
+
+@author: bvikram2
+'''
+'''
+Constructor
+'''
+
+
+class clasName:
+
+    # Constructor
+    def __init__(self):
+        print("inside contructor")
+
+    # mormal Nethod
+    def normalMethod(self):
+        print("Normal Method")
+
+
+a = clasName()
+a.normalMethod()
