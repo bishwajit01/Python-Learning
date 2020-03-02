0c04bb267840de002d64bc65d24063d4eb6b15ee
bishwajit01 <bishwajit.vikram@gmail.com>
2020-03-02 21:51:34 +0530

    Output Program 18.

---
+++ PythonPrograms/outputExercise/Program18.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 2, 2020
+
+@author: bvikram2
+'''
+'''
+Program Output
+'''
+
+a = True
+b = False
+c = False
+
+if a or b and c:
+    print("Vikram")
+else:
+    print("Bishwajit")
