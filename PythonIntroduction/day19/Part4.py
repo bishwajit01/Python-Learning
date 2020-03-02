eb0ab5f38a38eab43808fbafda0526049f5fc67d
bishwajit01 <bishwajit.vikram@gmail.com>
2020-03-02 20:56:25 +0530

    More about NumPy.

---
+++ PythonIntroduction/day19/Part4.py
@@ -0,0 +1,37 @@
+'''
+Created on Mar 2, 2020
+
+@author: bvikram2
+'''
+'''
+NumPy
+    Numpy Array is mutable.
+'''
+
+import numpy as np
+
+# Single Dimension Array
+a = np.array([1, 2, 3, 4, 5, 6])
+print(a)
+print(type(a))
+
+# Negative Indexing
+print(a[-1])
+# Positive Indexing
+print(a[4])
+
+a[1] = 40
+print(a[1])
+
+# Double Dimension Array Indexing
+a = np.arange(1, 10).reshape(3, 3)
+print(a)
+print(a[1, 2])
+
+# Three Dimension Array Indexing
+a = np.arange(1, 9).reshape(2, 2, 2)
+print(a)
+print(a[1, 1, 1])
+print(a[0, 1, 1])
+print(a[1, 0, 1])
+print(a[1, 1, 0])
