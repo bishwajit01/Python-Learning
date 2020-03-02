0002bd1056d1cb8474c5c31c88430ce294ff87c1
bishwajit01 <bishwajit.vikram@gmail.com>
2020-03-02 21:17:57 +0530

    Appending into NumPy

---
+++ PythonIntroduction/day19/Part5.py
@@ -0,0 +1,30 @@
+'''
+Created on Mar 2, 2020
+
+@author: bvikram2
+'''
+'''
+NumPy
+'''
+import numpy as np
+
+# Single Dimension Array
+a = np.array([2, 6, 2, 5, 8, 2, 7])
+print(a)
+print(type(a))
+
+# appending in the single dimension array
+a = np.append(a, [6, 9, 2, 5, 8])
+print(a)
+
+# double dimension array
+a = np.arange(1, 10).reshape(3, 3)
+print(a)
+print(type(a))
+
+# appending in the double dimension array
+b = np.append(a, [[3, 7, 4]], axis=0)  # if axis =0, it is for the row. 
+print(b)
+
+b = np.append(a, [[4], [2], [7]], axis=1)  # if axis =0, it is for the row. 
+print(b)
