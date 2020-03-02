9349a9e38e0145f3b358d53de66ee7fdae400f1b
bishwajit01 <bishwajit.vikram@gmail.com>
2020-03-02 21:40:24 +0530

    Subsetting Usig NumPy.

---
+++ PythonIntroduction/day19/Part7.py
@@ -0,0 +1,35 @@
+'''
+Created on Mar 2, 2020
+
+@author: bvikram2
+'''
+'''
+NumPy
+'''
+
+import numpy as np
+
+# Single Dimension Array
+a = np.arange(1, 17).reshape(4, 4)
+print(a)
+print(type(a))
+
+# Slicing only 3X3
+b = a[0:3, 0:3]
+print(b)
+
+# Slicing only 2X2
+b = a[0:2, 0:2]
+print(b)
+
+# Slicing only 2X2
+b = a[2:4, 2:4]
+print(b)
+
+# Slicing only 4X4
+b = a[:4, :4]
+print(b)
+
+# Slicing only 2X2
+b = a[2:, 2:]
+print(b)
