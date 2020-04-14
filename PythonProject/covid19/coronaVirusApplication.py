439eefd39ca87a1e6fc6079de5ff1091437d997b
bishwajit01 <bishwajit.vikram@gmail.com>
2020-04-14 10:22:28 +0530

    reading the csv file

--- PythonProject/covid19/coronaVirusApplication.py
+++ PythonProject/covid19/coronaVirusApplication.py
@@ -7,6 +7,7 @@ Created on Mar 30, 2020
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import matplotlib.pyplot as pl
 
 
 uri = "https://www.worldometers.info/coronavirus/"
@@ -54,3 +55,36 @@ dataFrames = pd.DataFrame(prepared_list, columns=headers)
 
 dataFrames.to_csv("C:\\Users\\bvikram2\\Desktop\\data.csv", index=False)
 print("==========Writing to a file Done==================")
+
+print("***********Reading from File**************")
+dataFrame = pd.read_csv("C:\\Users\\bvikram2\\Desktop\\data.csv")
+
+print(dataFrame)
+
+print("***********Reading Top 5 from File**************")
+print(dataFrame.head())
+print("***********Reading Last 5 from File**************")
+print(dataFrame.tail())
+
+newData = dataFrame[0:10]
+countries = newData["Countries"]
+print("***********COUNTRIES************")
+print(countries)
+x = list(countries)
+totalCase = newData["Total Cases"]
+print("***********TOTAL CASES************")
+print(totalCase)
+y = list(totalCase)
+
+print("***********PLOTTING THE BAR-GRAPHS************")
+pl.bar(x[::-1],y[::-1])
+pl.xlabel("Countries")
+pl.ylabel("TOTAL CASE")
+pl.title("CORONA TRACKER")
+pl.xticks(rotation=60)
+
+print("***********PLOTTING THE SCATTERED-GRAPHS************")
+pl.scatter(x[::-1],y[::-1])
+pl.xlabel("Countries")
+pl.ylabel("TOTAL CASE")
+pl.title("CORONA TRACKER")
